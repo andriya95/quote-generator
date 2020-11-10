adef5d2d08c61aaaf8545bde035a2f25393dcc52
andriya95 <andriyastokic@gmail.com>
2020-11-10 02:59:44 +0100

    finished

--- script.js
+++ script.js
@@ -1,14 +1,67 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader')
+
+//Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//Hide Loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
 // Get Quote From API
 async function getQuote() {
+    loading();
     const apiUrl= 'https://freequote.herokuapp.com/';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        console.log(data);
+        
+        //If author is blank, add 'Unknown'
+        if (data.author === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.author
+        }
+        
+        //Reduce font size for long quotes
+        if (data.quote.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+
+        quoteText.innerText = data.quote;
+        //Stop loader, show the quote
+        complete()
     } catch (error) {
-        console.log('whooops, no quote', error);
+
     }
 }
+
+//TweetQuote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 
 //On Load
 getQuote();
