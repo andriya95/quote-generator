62e788fed70acbb6c055bed3845dd851bf56c19d
andriya95 <andriyastokic@gmail.com>
2020-11-11 22:48:27 +0100

    finished basic look

--- script.js
+++ script.js
@@ -5,63 +5,60 @@ const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader')
 
-//Show loading
-function loading() {
-    loader.hidden = false;
-    quoteContainer.hidden = true;
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-//Hide Loading
-function complete() {
-    if (!loader.hidden) {
-        quoteContainer.hidden = false;
-        loader.hidden = true;
-    }
+function removeLoadingSpinner() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
 }
 
 // Get Quote From API
 async function getQuote() {
-    loading();
-    const apiUrl= 'https://freequote.herokuapp.com/';
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        
-        //If author is blank, add 'Unknown'
-        if (data.author === '') {
-            authorText.innerText = 'Unknown'
-        } else {
-            authorText.innerText = data.author
-        }
-        
-        //Reduce font size for long quotes
-        if (data.quote.length > 120) {
-            quoteText.classList.add('long-quote');
-        } else {
-            quoteText.classList.remove('long-quote');
-        }
-        quoteText.innerText = data.quote;
- 
-        //Stop loader, show the quote
-        complete()
-    } catch (error) {
+  showLoadingSpinner();
+  const apiUrl = 'https://freequote.herokuapp.com/';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
+    //Check If Author is blank and replace it with 'Unknown'
+    if (data.author === '') {
+      authorText.innerText = 'Unknown'
+    } else {
+      authorText.innerText = data.author
     }
+
+    //Dynamically reduce font size for long quotes
+    if (data.quote.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quote;
+
+    removeLoadingSpinner()
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+//On Load
+getQuote();
 
 //TweetQuote
 function tweetQuote() {
-    const quote = quoteText.innerText;
-    const author = authorText.innerText;
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
-    window.open(twitterUrl, '_blank');
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, '_blank');
 
 }
 
 //Event Listeners
 newQuoteBtn.addEventListener('click', getQuote);
 twitterBtn.addEventListener('click', tweetQuote);
-
-
-//On Load
-getQuote();
